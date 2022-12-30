92ec64390b67f310ecd84f6135c157a0c8d3e2b3
Govinda Agrawal <agrawalgovinda02@gmail.com>
2022-12-30 13:58:13 +0530

    finished django prac

--- prac/django/airliens/users/views.py
+++ prac/django/airliens/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+def login(request):
+    return render(request, "users/login.html")
+
+def logout(request):
+    pass
